72465054a374e80c93211f7b5fd196d3849dd477
Enzo Dias Lopes <105835159+EnzoDiasLopes23@users.noreply.github.com>
2024-12-10 09:42:27 -0300

    feat(p3): tabela para importação dos dados

--- sqript.sql
+++ sqript.sql
@@ -1,6 +1,6 @@
 CREATE TABLE students (
     id SERIAL PRIMARY KEY,
-    student_id VARCHAR(50) NOT NULL,
+    student_id VARCHAR(25) NOT NULL,
     grade INT,
     mother_edu INT,
     father_edu INT,
